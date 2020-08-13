5f4d73014d9a2067d4b96e79998795166b0f2e16
rizky sam pratama <rizkysampratama@gmail.com>
2020-08-13 20:04:17 +0700

    constant

---
+++ consttans.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	const firstname string = "Sam"
+	const lastname = "Pratama"
+	const value = 1000
+
+	fmt.Println(firstname)
+
+	// multiple
+	const (
+		country      = "Indonesia"
+		jml_province = 34
+	)
+}
