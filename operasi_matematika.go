eebab5aca3e449fb8ee4391bcb27fec5b9c4e019
rizky sam pratama <rizkysampratama@gmail.com>
2020-08-16 10:43:50 +0700

    operasi matematika

---
+++ operasi_matematika.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	var result = 10 + 20
+
+	var a = 10
+	var b = 50
+
+	fmt.Println(result)
+	fmt.Println(a * b)
+
+	var i = 10
+	i += 20
+	i++
+	fmt.Println(i)
+
+	var isHandsome = true
+
+	fmt.Println(!isHandsome)
+}
