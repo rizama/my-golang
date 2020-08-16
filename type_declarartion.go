0186a9dab9def36c8d92d9438b1f79b94343dd38
rizky sam pratama <rizkysampratama@gmail.com>
2020-08-16 10:34:27 +0700

    type decalaration

---
+++ type_declarartion.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// Alias tipe data untuk Variable
+func main() {
+	type noKtp string
+	tyoe isMarried bool
+
+	var ktpSam noKtp = "123456789"
+	var maried isMarried = true
+	fmt.Println(ktpSam)
+	fmt.Println(maried)
+}
