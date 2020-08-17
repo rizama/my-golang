c3369473dc8e7d2054d126a5b0fb5d28266e62b2
rizky sam pratama <rizkysampratama@gmail.com>
2020-08-17 13:01:39 +0700

    comparation

---
+++ operasi_perbandingan.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name1 = "sam"
+	var name2 = "sam"
+
+	var result = name1 == name2
+
+	fmt.Println(result)
+
+	var value1 = 1
+	var value2 = 2
+	fmt.Println(value1 > value2)
+}
