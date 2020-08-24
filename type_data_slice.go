06a1c1bc3c08d00ec3bd2e7407d020219790cf4f
rizky sam pratama <rizkysampratama@gmail.com>
2020-08-24 14:22:41 +0700

    End of Type Slice

--- type_data_slice.go
+++ type_data_slice.go
@@ -24,4 +24,38 @@ func main() {
 	fmt.Println("panjang slice1: ", len(slice1))
 	fmt.Println("kapasitas slice1: ", cap(slice1))
 
+	// months[5] = "Sam"
+	// fmt.Println(slice1)
+
+	// slice1[0] = "Pratama"
+	// fmt.Println(months)
+
+	var slice2 = months[10:]
+	fmt.Println(slice2)
+
+	var slice3 = append(slice2, "Rizky")
+	fmt.Println(slice3)
+	slice3[1] = "Bukan_desember"
+	fmt.Println(slice3)
+
+	fmt.Println(slice2)
+	fmt.Println(months)
+
+	newSlice := make([]string, 2, 5)
+	newSlice[0] = "MSI"
+	newSlice[1] = "ASUS"
+	var hehe = append(newSlice, "Sam")
+
+	fmt.Println(newSlice)
+	fmt.Println(len(newSlice))
+	fmt.Println(cap(newSlice))
+	fmt.Println(hehe)
+
+	copySlice := make([]string, len(newSlice), cap(newSlice))
+	copy(copySlice, newSlice)
+	fmt.Println(copySlice)
+
+	var iniArray = [...]int{1, 2, 3, 4, 5}
+	var iniSlice = []int{1, 2, 3, 4, 5}
+
 }
