ade49920124c8c5e80df9ffc494d3f7a23bc580d
rizky sam pratama <rizkysampratama@gmail.com>
2020-08-29 21:58:19 +0700

    multiple return function

--- function.go
+++ function.go
@@ -14,8 +14,19 @@ func sayMyAge(age int) int {
 	return age
 }
 
+func sayMyAddress(province string, city string) (string, string) {
+	return province, city
+}
+
+func whatMyAddress() (string, string) {
+	return "sadang serang", "cikutra"
+}
+
 func main() {
 	saySomething()
 	sayMyName("Rizky")
 	fmt.Println(sayMyAge(24))
+	fmt.Println(sayMyAddress("Jawa Barat", "Bandung"))
+	provinsi, _ := whatMyAddress()
+	fmt.Println(provinsi)
 }
