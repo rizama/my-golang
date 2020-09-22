3a409209df43e3eaad443f50991c47069905822f
Rizky Sam Pratama <rizkysampratama@gmail.com>
2020-09-22 23:24:46 +0700

    interface #1

---
+++ basic-golang/interface.go
@@ -0,0 +1,6 @@
+/**
+Interface:
+- tipe data yang abstract, dia tidak memiliki implementasi langsung
+- sebuah interface berisikan definisi-definisi method
+- biasanya interface digunakan sebagai kontrak
+*/
