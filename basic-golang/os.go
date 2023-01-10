377b263ea9cc5fb3e1c7c420672f0bb01252703a
rizky.pratama <rizky.pratama@sicepat.com>
2023-01-10 16:48:30 +0700

    update desc

--- basic-golang/os.go
+++ basic-golang/os.go
@@ -5,6 +5,12 @@ import (
 	"os"
 )
 
+/**
+Go-Lang telah menyediakan banyak sekali package bawaan, salah satunya adalah package os
+Package os berisikan fungsionalitas untuk mengakses fitur sistem operasi secara independen (bisa digunakan  disemua sistem operasi)
+https://golang.org/pkg/os/
+*/
+
 func main() {
 
 	// argumen yagn di passing dari code run
