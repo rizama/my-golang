b66515f9cc66134f38b72dbe42b97ead4f47391e
rizky.pratama <rizky.pratama@sicepat.com>
2023-01-10 15:03:43 +0700

    access modifier

--- basic-golang/helper/sayhello.go
+++ basic-golang/helper/sayhello.go
@@ -2,7 +2,15 @@
 
 package helper
 
-// nama Function Package harus Huruf besar awalnya (Camel Case)
+var version = "1.0.0"              // => tidak bisa diakses dari luar Package
+var Application = "Belajar Golang" // => bisa diakses dari luar Package
+
+// nama Function Package harus Huruf besar awalnya (Camel Case) agar dapat di akses oleh Package lain.
 func SayHello(name string) string {
 	return "Hello " + name
+}
+
+// tidak bisa diakses dari luar Package, karena huruf awalnya kecil.
+func sayGoodBye(name string) string {
+	return "Bye " + name
 }
--- basic-golang/import.go
+++ basic-golang/import.go
@@ -11,4 +11,11 @@ func main() {
 	result := helper.SayHello(name)
 
 	fmt.Println(result)
+
+	// helper.sayGoodBye(name) // => error, fungsi  sayGoodBye tidak di export
+	helper.SayHello(name) // => berfungsi SayHello sudah di export, karena memakai huruf awal besar
+
+	// fmt.Println(helper.version) //=> error, variable version tidak di export
+	fmt.Println(helper.Application) //=> variable Application sudah di export
+
 }
