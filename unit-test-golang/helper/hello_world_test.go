54e3552bbe03c2d198d2e5101405afaf153fc1ef
rizama <rizkysampratama@gmail.com>
2023-02-05 14:57:55 +0700

    assertion library

--- unit-test-golang/helper/hello_world_test.go
+++ unit-test-golang/helper/hello_world_test.go
@@ -3,12 +3,33 @@ package helper
 import (
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
+
+func TestHelloWorldAssert(t *testing.T) {
+	result := HelloWorld("World")
+
+	// assert.Equal(t, expected, testing function, message error)
+	assert.Equal(t, "Hello World", result, "Harusnya Hello World")
+
+	fmt.Println("Done")
+}
+
+func TestHelloWorldRequire(t *testing.T) {
+	result := HelloWorld("Worlds")
+
+	// assert.Equal(t, expected, testing function, message error)
+	require.Equal(t, "Hello World", result, "Harusnya Hello World")
+
+	fmt.Println("Done")
+}
 
 func TestHelloWorld(t *testing.T) {
 	result := HelloWorld("World")
 
-	if result != "Hello Worlds" {
+	if result != "Hello World" {
 		// Error
 		// t.Fail()
 		t.Error("Harusnya Hello Worlds")
@@ -20,7 +41,7 @@ func TestHelloWorld(t *testing.T) {
 func TestHelloWorldSam(t *testing.T) {
 	result := HelloWorld("Sam")
 
-	if result != "Hello Samd" {
+	if result != "Hello Sam" {
 		// Error
 		// t.FailNow()
 		t.Fatal("Harusnya Hello Samd")
