c11ec0bacf93daf5333819fa69619116fd7f490a
rizky.pratama <rizky.pratama@sicepat.com>
2023-02-21 14:45:34 +0700

    New Section - Context Golang

---
+++ belajar-golang-context/context_test.go
@@ -0,0 +1,16 @@
+package belajar_golang_context
+
+import (
+	"context"
+	"fmt"
+	"testing"
+)
+
+func TestContext(t *testing.T) {
+	background := context.Background()
+	fmt.Println(background)
+
+	todo := context.TODO()
+	fmt.Println(todo)
+
+}
