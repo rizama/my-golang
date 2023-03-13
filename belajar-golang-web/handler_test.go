8a796abfaabc744c56afd369d8f7d004d0cafea7
rizky.pratama <rizky.pratama@sicepat.com>
2023-03-13 11:03:49 +0700

    golang web

---
+++ belajar-golang-web/handler_test.go
@@ -0,0 +1,54 @@
+package belajar_golang_web
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func TestHandler(t *testing.T) {
+	var handler http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
+		// Logic Web
+		fmt.Fprint(w, "Hello Go World")
+	}
+
+	server := http.Server{
+		Addr:    "localhost:8081",
+		Handler: handler,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestServeMux(t *testing.T) {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello Golang World")
+	})
+
+	mux.HandleFunc("/sam", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello Sam")
+	})
+
+	mux.HandleFunc("/images/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "images")
+	})
+
+	mux.HandleFunc("/images/thumbnails/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "images/thumbnails")
+	})
+
+	server := http.Server{
+		Addr:    "localhost:8081",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
