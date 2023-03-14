25a044c8fff21034ec1143218d6bef6f50d96aa0
rizky.pratama <rizky.pratama@sicepat.com>
2023-03-14 14:39:26 +0700

    form post

---
+++ belajar-golang-web/form_post_test.go
@@ -0,0 +1,37 @@
+package belajar_golang_web
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func FormPost(writer http.ResponseWriter, request *http.Request) {
+	err := request.ParseForm()
+	if err != nil {
+		panic(err)
+	}
+
+	first := request.PostForm.Get("first") // cara 1, harus diparsing dulu
+	last := request.PostFormValue("last")  // cara 2, tanpa parsing
+
+	fmt.Fprintf(writer, "Hello %s %s", first, last)
+}
+
+func TestFormPost(t *testing.T) {
+	requestBody := strings.NewReader("first=sam&last=pratama")
+	request := httptest.NewRequest(http.MethodPost, "http://localhost:8081/posts", requestBody)
+	request.Header.Add("content-type", "application/x-www-form-urlencoded")
+	recorder := httptest.NewRecorder()
+
+	FormPost(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+	bodyString := string(body)
+
+	fmt.Println(bodyString)
+}
