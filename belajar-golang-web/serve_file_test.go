91fb0eac1a8a2b5a3ecc5d94f4d04c28b0d9641d
rizky.pratama <rizky.pratama@sicepat.com>
2023-06-23 14:56:09 +0700

    serve file

---
+++ belajar-golang-web/serve_file_test.go
@@ -0,0 +1,42 @@
+package belajar_golang_web
+
+import (
+	_ "embed"
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func ServeFile(writer http.ResponseWriter, request *http.Request) {
+	if request.URL.Query().Get("name") != "" {
+		http.ServeFile(writer, request, "./resources/ok.html")
+	} else {
+		http.ServeFile(writer, request, "./resources/notfound.html")
+	}
+}
+
+func TestServeFileServer(t *testing.T) {
+	server := http.Server{
+		Addr:    "localhost:8081",
+		Handler: http.HandlerFunc(ServeFileEmbed),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+//go:embed resources/ok.html
+var resourcesOk string
+
+//go:embed resources/notfound.html
+var resourcesNotfound string
+
+func ServeFileEmbed(writer http.ResponseWriter, request *http.Request) {
+	if request.URL.Query().Get("name") != "" {
+		fmt.Fprint(writer, resourcesOk)
+	} else {
+		fmt.Fprint(writer, resourcesNotfound)
+	}
+}
