6496979704e51a002009fd3f47bf316a636929ae
rizky.pratama <rizky.pratama@sicepat.com>
2023-06-30 15:08:34 +0700

    pengenalan http router

---
+++ belajar-golang-httprouter/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func main() {
+	router := httprouter.New()
+	router.GET("/", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		fmt.Fprint(w, "Hello Httprouter")
+	})
+
+	server := http.Server{
+		Addr:    "localhost:3000",
+		Handler: router,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
