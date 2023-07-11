c7e0b25b37bb2101c51ac21eb05f101da8c12fc9
rizky.pratama <rizky.pratama@sicepat.com>
2023-07-11 09:38:22 +0700

    middleware http router

---
+++ belajar-golang-httprouter/middleware_test.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+)
+
+// declare middleware struct
+type LogMiddleware struct {
+	http.Handler
+}
+
+// declare middleware function
+func (middleware *LogMiddleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Request Accepted")
+	middleware.Handler.ServeHTTP(w, r)
+}
+
+func TestMiddleware(t *testing.T) {
+	router := httprouter.New()
+	router.GET("/", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		fmt.Fprint(w, "Hello Middleware")
+	})
+
+	// Impl middleware
+	middleware := LogMiddleware{router}
+
+	request := httptest.NewRequest("GET", "http://localhost:3000/", nil)
+	recorder := httptest.NewRecorder()
+
+	middleware.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	assert.Equal(t, "Hello Middleware", string(body))
+}
